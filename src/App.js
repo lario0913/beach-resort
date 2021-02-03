3ca7ccbd390300d96195d990730be0498e2552ef
Lario <osunkunleolanrewaju@gmail.com>
2021-02-03 15:30:58 +0100

    added the navbar component of the application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './App.css'
+import {Route, Switch} from 'react-router-dom'
 
 import Home from './pages/Home'
 import Room from './pages/Room'
 // import Error from './pages/Error'
 import SingleRoom from './pages/SingleRoom'
 
-import {Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/rooms" exact component={Room} />
