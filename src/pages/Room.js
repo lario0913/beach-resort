b37af7967acd7ee6e74cb1480e1cb16afa7c65d1
Lario <osunkunleolanrewaju@gmail.com>
2021-02-04 22:16:16 +0100

    start working on the hero component

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 function Room() {
     return (
         <div>
-            Room
+            <Hero hero="roomsHero"/>
         </div>
     )
 }
